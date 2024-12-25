6fd3f42388360a1c647b2dc37da2587b3aaf7aac
satish v <satish.vakatisatyadha@gmail.com>
2024-12-25 02:06:04 +0000

    Commit

--- models/staging/stg_emp.sql
+++ models/staging/stg_emp.sql
@@ -2,7 +2,9 @@
 
 WITH tb1 as (
     select EMPID as ID, 
-    EMPNAME AS NAME
+    EMPNAME AS NAME,
+    SALARY as SAL,
+    LOCATION as LOC,
      from {{source('datafeed_shared_schema','STG_EMPL')}}
 )
 select * from tb1 
